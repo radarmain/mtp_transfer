059d6dc7b4fc9aac5c8cb1b17ad6a11903981191
william staley <williamcstaley@gmail.com>
2026-02-26 09:46:50 -0500

    Force https redirect URI in production

--- src/cognitoConfig.js
+++ src/cognitoConfig.js
@@ -1,6 +1,6 @@
 export const COGNITO_DOMAIN = "https://us-east-2n0cly7iq6.auth.us-east-2.amazoncognito.com";
 export const COGNITO_CLIENT_ID = "2dg67371rh474eorncugcubu3n";
 export const REDIRECT_URI =
-  window.location.origin === "http://localhost:5173"
+  window.location.hostname === "localhost"
     ? "http://localhost:5173/callback"
-    : `${window.location.origin}/callback`;
+    : `https://${window.location.host}/callback`;
